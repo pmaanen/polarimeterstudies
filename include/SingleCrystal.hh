0ffd2e970484717ecbfd24920f33fb6a68c05045
pmaanen <paul.maanen@rwth-aachen.de>
2015-09-09 15:47:14 +0200

    Improvements to SingleCrystal, CosmicSetup
    Moved possibility to specify rotation to parent class, as it can the
    useful there. Trigger detectors now are constructed when valid
    dimensions are given. Added a configurable offset for the trigger
    detector.

--- include/SingleCrystal.hh
+++ include/SingleCrystal.hh
@@ -14,9 +14,24 @@ class SingleCrystal: public JediPolarimeter {
 public:
 	SingleCrystal();
 	virtual ~SingleCrystal();
+	void setPhi(G4double phi) {
+		this->phi = phi;
+		changedParameters=true;
+	}
 
+	void setPsi(G4double psi) {
+		this->psi = psi;
+		changedParameters=true;
+	}
+
+	void setTheta(G4double theta) {
+		this->theta = theta;
+		changedParameters=true;
+	}
 protected:
 	virtual G4LogicalVolume* MakeCaloCrystal();
 	virtual G4VPhysicalVolume* Construct();
+	virtual void DefineCommands();
+	G4double theta,phi,psi;
 };
 #endif
