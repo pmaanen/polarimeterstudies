1dfb748827023fdcf8ec54b1a65e60bb8fd8297a
pmaanen <paul.maanen@rwth-aachen.de>
2015-10-28 12:32:09 +0100

    scripts for homogenste

--- scripts/homogenity.sh
+++ scripts/homogenity.sh
@@ -8,4 +8,5 @@ fi
 echo "N="$N
 mkdir -p sim/homogenity-muon
 ./polarimeterstudies -g cosmic: -m scripts/homogenity.mac -n $N -b 2>/dev/null 1>/dev/null
-cp ./scripts/analyse.py sim/homogenity-muon
+cp ./scripts/analyse-homogenity.py sim/homogenity-muon
+cd sim/homogenity-muon; ./analyse-homogenity.py; cd -
