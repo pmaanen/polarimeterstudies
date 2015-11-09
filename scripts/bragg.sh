1edaf868194204698492fda35ba0ba7b8730b02c
maanen <maanen@physik.rwth-aachen.de>
2015-11-09 16:25:05 +0100

    fixed scripts. analysis now works. StepLimiter set to more sensible values.

--- scripts/bragg.sh
+++ scripts/bragg.sh
@@ -7,5 +7,5 @@ fi
 echo "N="$N
 mkdir -p sim/bragg
 ./polarimeterstudies -g single: -m scripts/bragg-all.mac -n $N -b 2>/dev/null 1>/dev/null
-cp ./scripts/analyse.py sim/bragg/.
-cd sim/bragg  && ./analyse.py *.root && cd -
+cp ./scripts/analyse-bragg.py sim/bragg/.
+cd sim/bragg  && ./analyse-bragg.py *.root && cd -
