77329c2913b874075a562b08445e5e4c4aea2a11
maanen <maanen@physik.rwth-aachen.de>
2015-11-09 12:14:57 +0100

    analysis and fits work now.

--- scripts/analyse-homogenity.py
+++ scripts/analyse-homogenity.py
@@ -10,6 +10,11 @@ class Gaus:
    def __call__( self, x, p ):
        p[0]*ROOT.TMath.Gaus(x[0],p[1],p[2])
 
+#class Landau:
+#   def __call__( self, x, p ):
+#       p[0]*ROOT.TMath.Gaus(x[0],p[1],p[2])
+
+
 class hit:
     def __init__(self,hit):
         self.detid=hit.detid
@@ -21,14 +26,9 @@ class chit:
       self.nphot=hit.nphot
       self.event=hit.event
 
-def doEvent(calo,cathode,histo):
-    if len(calo)==0:
-        print "no hits, event skipped"
-        return
-    
-    if len(calo)!=1 or len(cathode)!=1 or calo[0].event!=cathode[0].event:
-    #    print "malformed event, event skipped"
-        return
+def doEvent(cathode,histo):
+   histo.Fill(cathode[0].nphot)
+   return
     
 
 def getOneEvent(EventIndex,EventList):
@@ -43,51 +43,52 @@ def getOneEvent(EventIndex,EventList):
     return result
     
 def doFile(filename):
-    nphot=ROOT.TH1F("nphot","N_{#gamma}",300,0,300)
+    nphot=ROOT.TH1F("nphot","N_{#gamma}",500,0,5e5)
     nphot.GetYaxis().SetTitle("#")
     nphot.GetXaxis().SetTitle("N_{#gamma}")
     infile=ROOT.TFile(filename,"UPDATE")
     calorimeter=infile.Get("Calorimeter")
-    trigger=infile.Get("Cathode")
+    cathode=infile.Get("Cathode")
     
     calorhits=[]
     cathodehits=[]
-    for ihit in calorimeter:
-        calorhits.append(hit(ihit))
-    for ihit in trigger:
+    for ihit in cathode:
         cathodehits.append(chit(ihit))
-    calorhits.sort(key=lambda hit: hit.event)
     cathodehits.sort(key=lambda hit: hit.event)
-
-        
-    thisEventCalor=[]
     thisEventCathode=[]
     while True:
-        iEvent=calorhits[-1].event
-        thisEventCalor=getOneEvent(iEvent,calorhits)
+        iEvent=cathodehits[-1].event
         thisEventCathode=getOneEvent(iEvent,cathodehits)
-        doEvent(thisEventCalor,thisEventCathode,nphot)
-        if len(calorhits)==0 or len(cathodehits)==0:
+        doEvent(thisEventCathode,nphot)
+        if len(cathodehits)==0:
             break
+    fitfunc=ROOT.TF1("fitfunc","[0]*TMath::Gaus(x,[1],[2])",nphot.GetMean()-nphot.GetRMS(),nphot.GetMean()+nphot.GetRMS())
+    fitfunc.SetParameter(0,nphot.GetBinContent(nphot.FindBin(nphot.GetMean())))
+    fitfunc.SetParameter(1,nphot.GetMean())
+    fitfunc.SetParameter(2,nphot.GetRMS())
+    nphot.Fit(fitfunc,"QRM")
     nphot.Write()
-    return nphot.GetMean()
+    return [fitfunc.GetParameter(1),fitfunc.GetParError(1)]
 def main():
     nphot_mean=[]
+    nphot_err=[]
     zPos=range(5,100,10)
     for z in zPos:
         filename="muons_"+str(z)+".root"
         print "analysing "+str(filename)
         try:
-            nphot_mean.append(doFile(filename))
+           result=doFile(filename)
+           nphot_err.append(result[1])
+           nphot_mean.append(result[0])
         except:
             print "Error while analysing "+filename+":"
             raise
-    for i in len(zPos):
+    for i in range(len(zPos)):
        zPos[i]=float(zPos[i])*1.0
-    myGraph=ROOT.TGraph(len(nphot_mean),asarray(zPos),asarray(nphot_mean))
+    myGraph=ROOT.TGraphErrors(len(nphot_mean),asarray(zPos),asarray(nphot_mean),asarray(len(nphot_mean)*[0]),asarray(nphot_err))
     myGraph.GetYaxis().SetTitle("<N_{#gamma}>")
     myGraph.GetXaxis().SetTitle("z [mm]")
     c1=ROOT.TCanvas("","",800,600)
-    myGraph.Draw("ALP")
+    myGraph.Draw("AP")
     c1.Print("c1.root")
 main()
