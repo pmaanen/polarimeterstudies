48ba22a2513e3ea598fffc9269860354cf333434
Paul Maanen <paul.maanen@rwth-aachen.de>
2016-03-03 16:54:41 +0100

    removed nunneeded materials from simulation. fixes to bragg.sh etc

--- scripts/bragg.sh
+++ scripts/bragg.sh
@@ -5,13 +5,13 @@ if [ $# -eq 1 ]
     N="$1"
 fi
 echo "N="$N
-mkdir -p sim/bragg
+mkdir -p ~/sim/bragg
 ./polarimeterstudies -g single: -m scripts/bragg.mac -n $N -b
-mv sim/bragg/ ~/sim/bragg
+mv *root ~/sim/bragg
 cp ./scripts/analyse-bragg.py ~/sim/bragg/.
-#cp ./scripts/CalculateLateral.py sim/bragg/.
-#cp ./scripts/CalculateThickness.py sim/bragg/.
-#cp ./scripts/CalculateRange.py sim/bragg/.
+cp ./scripts/CalculateLateral.py sim/bragg/.
+cp ./scripts/CalculateThickness.py sim/bragg/.
+cp ./scripts/CalculateRange.py sim/bragg/.
 cp ./scripts/AnalysisBase.py ~/sim/bragg
 cp ./scripts/analyse-bragg.py ~/sim/bragg
 cp ./libAnalysis* ~/sim/bragg
