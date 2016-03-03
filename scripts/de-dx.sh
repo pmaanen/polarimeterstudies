26860d9503edd2b3bf93c8f27ece8f549ebcceb2
Paul Maanen <paul.maanen@rwth-aachen.de>
2016-03-03 16:48:25 +0100

    minor fixes to scripts

--- scripts/de-dx.sh
+++ scripts/de-dx.sh
@@ -4,10 +4,9 @@ if [ $# -eq 1 ]
   then
     N="$1"
 fi
-export PYTHONPATH+=$(pwd)/scripts
 mkdir -p ~/sim/de-dx
-./polarimeterstudies -g testbench: -m scripts/de-dx.mac -n $N -b -o
-find ./ -name "*root" -type f -d 1 -exec {} mv ~/sim/de-dx \;
+./polarimeterstudies -g testbench: -m scripts/de-dx.mac -n $N -b
+mv *root ~/sim/de-dx
 cp ./scripts/analyse-de-dx.py ~/sim/de-dx/.
 cp ./scripts/AnalysisBase.py ~/sim/de-dx/.
 cp ./libAnalysis* ~/sim/de-dx/.
