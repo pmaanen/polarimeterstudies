7b0e2cc84c860c037ede31f2eef0a5c2f868179d
pmaanen <paul.maanen@rwth-aachen.de>
2016-04-04 10:23:38 +0200

    bugfixes

--- src/DCBreakupEventGenerator.cc
+++ src/DCBreakupEventGenerator.cc
@@ -17,11 +17,11 @@ static double DegToRad=3.14159265359/180.;
 //static double RadToDeg=1/DegToRad;
 
 DCBreakupEventGenerator::DCBreakupEventGenerator(G4ParticleGun* gun):PhaseSpaceGenerator(gun),fScatteringModel(nullptr) {
-	fBeamEnergy=270.*CLHEP::MeV;
+	fBeamEnergy=113.*CLHEP::MeV;
 	fInitialized=false;
 
-	fThetaMin=5.0*CLHEP::deg;
-	fThetaMax=20.*CLHEP::deg;
+	fThetaMin=0*CLHEP::deg;
+	fThetaMax=180.*CLHEP::deg;
 	DefineCommands();
 }
 
@@ -107,42 +107,39 @@ genevent_t DCBreakupEventGenerator::Generate() {
 		G4double Ex=0;
 		auto bound=neutron_4+carbon_4;
 
-		//Ex=(fBeam.Energy()-fBeam.M()-(proton_4.Energy()-proton_4.M()))*CLHEP::GeV;
-
-
-
 		auto beamCMS=fBeam;
 		beamCMS.Boost(-fCms.BoostVector());
 		auto protonCMS=proton_4;
-		proton_4.Boost(-fCms.BoostVector());
+		protonCMS.Boost(-fCms.BoostVector());
 		auto carbonCMS=carbon_4;
-		Ex=(fBeam.Energy()+fTarget.Energy()-proton_4.Energy()-bound.Energy())*CLHEP::GeV;
-		//Ex=((proton_4-fBeam).Energy())*CLHEP::GeV;//(fBeam.Energy()-fBeam.M()-(proton_4.Energy()-proton_4.M()))*CLHEP::GeV;
-		//G4cout<<"Ex="<<Ex/CLHEP::MeV<<G4endl;
-		//Set angular cut in lab-frame
-		if(th_scattered>fThetaMin and th_scattered<fThetaMax){
-			if(Ex<0)
-				continue;
-			G4double acc=100*G4UniformRand();
-			auto p3=proton_4.Vect();
-			auto c3=carbon_4.Vect();
-			auto n3=neutron_4.Vect();
-			G4ThreeVector proton_3(p3.X()*CLHEP::GeV,p3.Y()*CLHEP::GeV,p3.Z()*CLHEP::GeV);
-			G4ThreeVector carbon_3(c3.X()*CLHEP::GeV,c3.Y()*CLHEP::GeV,c3.Z()*CLHEP::GeV);
-			G4ThreeVector neutron_3(n3.X()*CLHEP::GeV,n3.Y()*CLHEP::GeV,n3.Z()*CLHEP::GeV);
-			if(fScatteringModel->sigma(th_scattered/CLHEP::deg,0,Ex/CLHEP::MeV)<acc){
-				continue;
-			}
-			else{
-				G4cout<<"DATA="<<Ex/CLHEP::MeV<<" "<<(proton_4.Energy()*CLHEP::GeV-proton_4.M()*CLHEP::GeV)/CLHEP::MeV<<" "<<(neutron_4.Energy()*CLHEP::GeV-neutron_4.M()*CLHEP::GeV)/CLHEP::MeV<<G4endl;
-				genevent_t res(0,0,pos.getX(),pos.getY(),pos.getZ());
-				G4cout<<"Ep="<<(proton_4.Energy()*CLHEP::GeV-proton_4.M()*CLHEP::GeV)/CLHEP::MeV<<G4endl;
-				res.particles.push_back(particle_t(fParticles[0]->GetPDGEncoding(),proton_3.getX()/CLHEP::GeV,proton_3.getY()/CLHEP::GeV,proton_3.getZ()/CLHEP::GeV,proton_4.Energy()*CLHEP::GeV-proton_4.M()*CLHEP::GeV));
-				res.particles.push_back(particle_t(fParticles[1]->GetPDGEncoding(),neutron_3.getX()/CLHEP::GeV,neutron_3.getY()/CLHEP::GeV,neutron_3.getZ()/CLHEP::GeV,neutron_4.Energy()*CLHEP::GeV-neutron_4.M()*CLHEP::GeV));
-				res.particles.push_back(particle_t(fParticles[2]->GetPDGEncoding(),carbon_3.getX()/CLHEP::GeV,carbon_3.getY()/CLHEP::GeV,carbon_3.getZ()/CLHEP::GeV,carbon_4.Energy()*CLHEP::GeV-carbon_4.M()*CLHEP::GeV));
-				return res;
-			}
-		}
+		carbonCMS.Boost(-fCms.BoostVector());
+		auto neutronCMS=neutron_4;;
+		neutronCMS.Boost(-fCms.BoostVector());
+		Ex=(neutronCMS.E()-neutronCMS.M())*CLHEP::GeV;
+		//G4cout<<G4BestUnit(Ex,"Energy")<<G4endl;
+				//Set angular cut in lab-frame
+				if(th_scattered>fThetaMin and th_scattered<fThetaMax){
+					if(Ex<0)
+						continue;
+					G4double acc=100*G4UniformRand();
+					auto p3=proton_4.Vect();
+					auto c3=carbon_4.Vect();
+					auto n3=neutron_4.Vect();
+					G4ThreeVector proton_3(p3.X()*CLHEP::GeV,p3.Y()*CLHEP::GeV,p3.Z()*CLHEP::GeV);
+					G4ThreeVector carbon_3(c3.X()*CLHEP::GeV,c3.Y()*CLHEP::GeV,c3.Z()*CLHEP::GeV);
+					G4ThreeVector neutron_3(n3.X()*CLHEP::GeV,n3.Y()*CLHEP::GeV,n3.Z()*CLHEP::GeV);
+					if(fScatteringModel->sigma(th_scattered/CLHEP::deg,0,Ex/CLHEP::MeV)<acc){
+						continue;
+					}
+					else{
+						genevent_t res(0,0,pos.getX(),pos.getY(),pos.getZ());
+						G4cout<<"Ep="<<(proton_4.Energy()*CLHEP::GeV-proton_4.M()*CLHEP::GeV)/CLHEP::MeV<<G4endl;
+						res.particles.push_back(particle_t(fParticles[0]->GetPDGEncoding(),proton_3.getX()/CLHEP::GeV,proton_3.getY()/CLHEP::GeV,proton_3.getZ()/CLHEP::GeV,proton_4.Energy()*CLHEP::GeV-proton_4.M()*CLHEP::GeV));
+						res.particles.push_back(particle_t(fParticles[1]->GetPDGEncoding(),neutron_3.getX()/CLHEP::GeV,neutron_3.getY()/CLHEP::GeV,neutron_3.getZ()/CLHEP::GeV,neutron_4.Energy()*CLHEP::GeV-neutron_4.M()*CLHEP::GeV));
+						res.particles.push_back(particle_t(fParticles[2]->GetPDGEncoding(),carbon_3.getX()/CLHEP::GeV,carbon_3.getY()/CLHEP::GeV,carbon_3.getZ()/CLHEP::GeV,carbon_4.Energy()*CLHEP::GeV-carbon_4.M()*CLHEP::GeV));
+						return res;
+					}
+				}
 	}
 }
 
