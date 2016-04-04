0e1e280979bb1578dcf4f2548189094e0a4a0454
pmaanen <paul.maanen@rwth-aachen.de>
2016-04-04 16:52:23 +0200

    put color def's in own header

---
+++ include/Colors.hh
@@ -0,0 +1,21 @@
+/*
+ * Colors.hh
+ *
+ *  Created on: 04.04.2016
+ *      Author: pmaanen
+ */
+
+#ifndef INCLUDE_COLORS_HH_
+#define INCLUDE_COLORS_HH_
+#include <G4Colour.hh>
+static G4Colour
+white   (1.0, 1.0, 1.0),  // white
+gray    (0.5, 0.5, 0.5), // gray
+black   (0.0, 0.0, 0.0), // black
+red     (1.0, 0.0, 0.0), // red
+green   (0.0, 1.0, 0.0), // green
+blue    (0.0, 0.0, 1.0), // blue
+cyan    (0.0, 1.0, 1.0), // cyan
+magenta (1.0, 0.0, 1.0), // magenta
+yellow  (1.0, 1.0, 0.0); // yellow
+#endif /* INCLUDE_COLORS_HH_ */
