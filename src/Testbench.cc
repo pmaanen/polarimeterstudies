2d0086869dade08ad68913ef7b4d3bb0d0c0b7ff
pmaanen <paul.maanen@rwth-aachen.de>
2016-08-26 01:36:21 +0200

    revert to old detector design

--- src/Testbench.cc
+++ src/Testbench.cc
@@ -11,7 +11,7 @@ Testbench::Testbench():SingleCrystal(),fLogicTrigger(0),fTriggerOffsetX(0),fTrig
 	fCrystalLength=10*CLHEP::cm;
 	fCrystalWidth=3*CLHEP::cm;
 	fTriggerHeight=2.5*CLHEP::cm;
-	fTriggerWidth=1.5*CLHEP::cm;
+	fTriggerWidth=2.5*CLHEP::cm;
 	fTriggerThickness=.5*CLHEP::cm;
 	fTrigger=true;
 	fScintillatorMaterialName="LYSO";
@@ -32,54 +32,62 @@ G4VPhysicalVolume* Testbench::Construct() {
 	fLogicWorld->SetVisAttributes(G4VisAttributes::Invisible);
 	fPhysiWorld=new G4PVPlacement(0,G4ThreeVector(0,0,0),fLogicWorld,"World",0,0,0,0);
 
-	auto solidTarget=new G4Box("Target",.5*CLHEP::cm,.5*CLHEP::cm,1*CLHEP::mm);
-	auto logicTarget=new G4LogicalVolume(solidTarget,G4NistManager::Instance()->FindOrBuildMaterial("G4_C"),"Target");
-	new G4PVPlacement(0,G4ThreeVector(0,0,0),logicTarget,"Target",fLogicWorld,0,0,0);
+	G4LogicalVolume* aCrystal=MakeCaloCrystal();
+	//fSensitiveDetectors.Update("Detector",SDtype::kCalorimeter,{aCrystal});
+	G4RotationMatrix* rot=new G4RotationMatrix();
+	rot->set(fPhi,fTheta,fPsi);
+	new G4PVPlacement (rot, G4ThreeVector(0,0,0), aCrystal, "Crystal", fLogicWorld, false, 0, false);
+	if(fTriggerWidth>0 and fTriggerHeight>0 and fTriggerThickness>0){
+		G4Box* solidTrigger=new G4Box("Trigger",fTriggerWidth/2,fTriggerHeight/2,fTriggerThickness/2);
+		auto triggerRot=new G4RotationMatrix();
+		triggerRot->rotateX(90*CLHEP::deg);
+		auto logicTrigger=new G4LogicalVolume(solidTrigger,G4NistManager::Instance()->FindOrBuildMaterial("G4_PLASTIC_SC_VINYLTOLUENE"),"Trigger");
+		logicTrigger->SetVisAttributes(new G4VisAttributes(G4Colour(1.,0.,0.)));
+		new G4PVPlacement (triggerRot, G4ThreeVector(0,0,-15*CLHEP::cm), logicTrigger, "Trigger", fLogicWorld, false, 0, false);
+		new G4PVPlacement (triggerRot, G4ThreeVector(0,0,-10*CLHEP::cm), logicTrigger, "Trigger", fLogicWorld, false, 1, false);
+		new G4PVPlacement (triggerRot, G4ThreeVector(0,0,15*CLHEP::cm), logicTrigger, "Trigger", fLogicWorld, false, 2, false);
+		fSensitiveDetectors.Update("Trigger",SDtype::kCalorimeter,{logicTrigger});
+	}
+		return fPhysiWorld;
+	}
 
-	auto solidDetector=new G4Tubs("Detector",3.5*CLHEP::mm,12.3*CLHEP::mm,.5*CLHEP::mm,0,360*CLHEP::deg);
-	auto logicDetector=new G4LogicalVolume(solidDetector,G4NistManager::Instance()->FindOrBuildMaterial("G4_Galactic"),"Detector");
-	new G4PVPlacement(0,G4ThreeVector(0,0,10*CLHEP::cm),logicDetector,"Detector",fLogicWorld,0,0,0);
+	void Testbench::DefineCommands() {
 
-	//fSensitiveDetectors.Update("Detector",SDtype::kPerfect,{logicDetector});
-
-	return fPhysiWorld;
-}
-
-void Testbench::DefineCommands() {
-
-	SingleCrystal::DefineCommands();
-	G4GenericMessenger::Command& triggerLengthCmd
-	= fMessenger->DeclareMethodWithUnit("triggerlength","mm",
-			&Testbench::setTriggerLength,
-			"trigger length (mm)");
-
-
-	G4GenericMessenger::Command& triggerThicknessCmd
-	= fMessenger->DeclareMethodWithUnit("triggerthickness","mm",
-			&Testbench::setTriggerThickness,
-			"trigger thickness (mm)");
-
-	G4GenericMessenger::Command& triggerWidthCmd
-	= fMessenger->DeclareMethodWithUnit("triggerwidth","mm",
-			&Testbench::setTriggerWidth,
-			"trigger width (mm)");
-
-	triggerWidthCmd.SetParameterName("width", true);
-	triggerWidthCmd.SetRange("width>=0.");
-	triggerWidthCmd.SetDefaultValue("30.");
-
-	G4GenericMessenger::Command& trgOffsetXCmd
-	= fMessenger->DeclareMethodWithUnit("trgOffsetX","mm",
-			&Testbench::setTriggerOffsetX,
-			"trigger offset in x dir. (mm)");
-	G4GenericMessenger::Command& trgOffsetYCmd
-	= fMessenger->DeclareMethodWithUnit("trgOffsetY","mm",
-			&Testbench::setTriggerOffsetY,
-			"trigger offset in y dir. (mm)");
-	G4GenericMessenger::Command& trgOffsetZCmd
-	= fMessenger->DeclareMethodWithUnit("trgOffsetZ","mm",
-			&Testbench::setTriggerOffsetZ,
-			"trigger offset in z dir. (mm)");
+		SingleCrystal::DefineCommands();
+		G4GenericMessenger::Command& triggerLengthCmd
+		= fMessenger->DeclareMethodWithUnit("triggerlength","mm",
+				&Testbench::setTriggerLength,
+				"trigger length (mm)");
 
 
-}
+		G4GenericMessenger::Command& triggerThicknessCmd
+		= fMessenger->DeclareMethodWithUnit("triggerthickness","mm",
+				&Testbench::setTriggerThickness,
+				"trigger thickness (mm)");
+
+		G4GenericMessenger::Command& triggerWidthCmd
+		= fMessenger->DeclareMethodWithUnit("triggerwidth","mm",
+				&Testbench::setTriggerWidth,
+				"trigger width (mm)");
+
+		triggerWidthCmd.SetParameterName("width", true);
+		triggerWidthCmd.SetRange("width>=0.");
+		triggerWidthCmd.SetDefaultValue("30.");
+
+		G4GenericMessenger::Command& trgOffsetXCmd
+		= fMessenger->DeclareMethodWithUnit("trgOffsetX","mm",
+				&Testbench::setTriggerOffsetX,
+				"trigger offset in x dir. (mm)");
+
+		G4GenericMessenger::Command& trgOffsetYCmd
+		= fMessenger->DeclareMethodWithUnit("trgOffsetY","mm",
+				&Testbench::setTriggerOffsetY,
+				"trigger offset in y dir. (mm)");
+
+		G4GenericMessenger::Command& trgOffsetZCmd
+		= fMessenger->DeclareMethodWithUnit("trgOffsetZ","mm",
+				&Testbench::setTriggerOffsetZ,
+				"trigger offset in z dir. (mm)");
+
+
+	}
