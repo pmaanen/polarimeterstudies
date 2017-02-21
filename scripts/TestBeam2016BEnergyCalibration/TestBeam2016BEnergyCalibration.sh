17a01532825f578cf1279d871f33ae9d9d9e317e
pmaanen <paul.maanen@rwth-aachen.de>
2017-02-21 23:34:50 +0100

    Scripts for Testbeam2016B energy calibration

---
+++ scripts/TestBeam2016BEnergyCalibration/TestBeam2016BEnergyCalibration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+N="4"
+NAME=TestBeam2016BEnergyCalibration
+if [ $# -eq 1 ]
+  then
+    N="$1"
+fi
+DIR=../$NAME-$(date "+%Y-%m-%d")
+mkdir -p $DIR
+nice -n 15 ./polarimeterstudies -g testbeam2016b -m scripts/$NAME/$NAME.mac -n $N 1>out.log 2>err.log
+mv *.root $DIR/
+cp scripts/AnalysisBase.py $DIR/.
+cp libAnalysis* $DIR/.
+cp scripts/$NAME/$NAME.py $DIR/.
+cd $DIR
+./$NAME.py -o $NAME.root -n $N 
