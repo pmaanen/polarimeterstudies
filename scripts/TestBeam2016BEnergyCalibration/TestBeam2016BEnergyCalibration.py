d63376cc71ae546ad685a672f6a98c5131d76aeb
Paul Maanen <paul.maanen@rwth-aachen.de>
2017-02-22 15:35:59 +0100

    fixed scripts for TestBeam2016BEnergyCalibration

--- scripts/TestBeam2016BEnergyCalibration/TestBeam2016BEnergyCalibration.py
+++ scripts/TestBeam2016BEnergyCalibration/TestBeam2016BEnergyCalibration.py
@@ -12,7 +12,8 @@ gStyle.SetMarkerStyle(20)
 gStyle.SetMarkerSize(2.5)
 gStyle.SetLabelSize(.045,"xy")
 gStyle.SetTitleSize(.05,"xy")
-gStyle.SetTitleOffset(.8,"xy")
+gStyle.SetTitleOffset(1,"xy")
+gStyle.SetOptFit(1)
 
 class exampleAnalysis(AnalysisBase):
     def Init(self):
@@ -37,7 +38,9 @@ def asfloatarray(vec):
 
 c1=ROOT.TCanvas("graph","graph",1600,900)
 def fitDir(infile,dirname):
-    histo=infile.Get(dirname+"/hEkin")
+    histo=infile.Get(dirname+"/hEdep").Clone()
+    histo.SetName("hEdep-Zoomed")
+    infile.cd(dirname)
     if histo.GetEntries()<1000:
         return None,None,None
     histo.Scale(1./histo.GetEntries())
@@ -45,9 +48,9 @@ def fitDir(infile,dirname):
     maximum=histo.GetMaximum()
     maximumLoc=histo.GetXaxis().GetBinCenter(histo.GetMaximumBin())
     width=max(1/2.3548*getFWHM(histo),histo.GetXaxis().GetBinWidth(histo.GetMaximumBin()))
-    histo.GetXaxis().SetRangeUser(max(0,maximumLoc-7*width),maximumLoc+7*width)
-    print maximumLoc,width,getFWHM(histo)
-    histo.GetXaxis().SetTitle("T_{d} [MeV]")
+    histo.GetXaxis().SetRangeUser(max(0,maximumLoc-5*width),maximumLoc+5*width)
+    histo.GetYaxis().SetRangeUser(0,1.2*maximum)
+    histo.GetXaxis().SetTitle("E_{dep} [MeV]")
     histo.GetYaxis().SetTitle("rel. freq.")
     fitfunc=ROOT.TF1("Gaussian","[0]*TMath::Gaus(x,[1],[2])",maximumLoc-4*width,maximumLoc+4*width)
     fitfunc.SetParName(0,"Constant")
@@ -56,90 +59,112 @@ def fitDir(infile,dirname):
     fitfunc.SetParameters(maximum,maximumLoc,width)
     fitfunc.SetNpx(1000)
     histo.Fit(fitfunc,"RQ")
-    if fitfunc.GetChisquare()/fitfunc.GetNDF()>100:
+    if fitfunc.GetChisquare()/fitfunc.GetNDF()>1e6:
         return None,None,None
     histo.SetTitle(dirname+" MeV")
     histo.Draw()
+    histo.Write()
     c1.Print(dirname+"-ekin.pdf")
     e=fitfunc.GetParameter(1)
     de=fitfunc.GetParError(1)
     w=2.3548*fitfunc.GetParameter(2)
     return e,de,w
 
-def makePlots(infile,material,e,d):
-    iy,idy,iw=fitDir(infile,material+"-"+str(e)+"-"+str(d))
-    if iy!=None:
-        x.append(d)
-        y.append(iy)
-        dy.append(idy)
-        w.append(iw)
-        myGraph=ROOT.TGraph(len(x))
+def makePlots(title,x,y,w):
+        gEdep=ROOT.TGraph(len(x))
+        gEdep.SetName(title[:-1])
+        gEdep.SetTitle("")
         for ix in range(len(x)):
-            myGraph.SetPoint(ix,x[ix],y[ix])
-        myGraph.SetLineColor(colors[iCol % len(colors)])
-        myGraph.SetMarkerColor(colors[(iCol +1 )% len(colors)])
-        myGraph.SetFillColor(colors[iCol % len(colors)])
-        myGraph.SetLineWidth(3)
-        myGraph.SetTitle("")
-        myGraph.Draw("ALP")
-        myGraph.GetXaxis().SetTitle("Target Thickness [mm]")
-        myGraph.GetYaxis().SetTitle("T_{d} [MeV]")
-        myGraph.Draw()
-        c1.Print(material+str(e)+"-energy.pdf")
+            gEdep.SetPoint(ix,x[ix],y[ix])
+        gEdep.SetLineColor(colors[iCol % len(colors)])
+        gEdep.SetMarkerColor(colors[(iCol +1 )% len(colors)])
+        gEdep.SetFillColor(colors[iCol % len(colors)])
+        gEdep.SetLineWidth(3)
+        gEdep.SetTitle("")
+        gEdep.Draw("ALP")
+        gEdep.GetYaxis().SetTitle("E_{dep} [MeV]")
+        gEdep.GetXaxis().SetTitle("T_{d} [MeV]")
+        gEdep.Draw()
+        c1.Print(title+"energy.pdf")
+        #c1.Print(title+"energy.root")
+        gEdep.Write()
+        gFWHM=ROOT.TGraph(len(x))
+        gFWHM.SetName(title[:-1])
+        gFWHM.SetTitle(title[:-1])
+        gFWHM.SetLineColor(colors[iCol % len(colors)])
+        gFWHM.SetMarkerColor(colors[(iCol +1 )% len(colors)])
+        gFWHM.SetFillColor(colors[iCol % len(colors)])
+        gFWHM.SetLineWidth(3)
         for ix in range(len(x)):
-            myGraph.SetPoint(ix,x[ix],w[ix])
-        myGraph.GetXaxis().SetTitle("Target Thickness [mm]")
-        myGraph.GetYaxis().SetTitle("T_{d} FWHM [MeV]")
-        myGraph.Draw("ALP")
-        c1.Print(material+"-"+str(d)+"-"+str(e)+"-FWHM.pdf")
-
+            gFWHM.SetPoint(ix,x[ix],w[ix])
+        gFWHM.GetXaxis().SetTitle("T_{d} [MeV]")
+        gFWHM.GetYaxis().SetTitle("#Delta E (FWHM) [MeV]")
+        gFWHM.Draw("ALP")
+        c1.Print(title+"FWHM.pdf") 
+        #c1.Print(title+"FWHM.root")
+        gFWHM.Write()
 class exampleAnalysis(AnalysisBase):
     def Init(self):
         return
     
-    def analysis(self,filename,myWorker):
-        try:
-            hSum=ROOT.TH1F("hSum","E_{dep}",300,0,300)
-            hSum.GetXaxis().SetTitle("E_{dep} [MeV]")
-            hSum.GetYaxis().SetTitle("rel. freq.")
-            outfile=ROOT.TFile(filename[:-5]+"-histos.root","RECREATE")
-            dir=outfile.mkdir(filename[:-5])
-            dir.cd()
-            infile=ROOT.TFile(filename,"READ")
-            data=infile.Get("sim")
-            dir.cd()
-            for event in data:
-                sum=0
-                for hit in event.Detector:
-                    sum+=hit.edep
-            outfile.Write()
-            outfile.Close()
-        except Exception as e:
-            print "Problem in file:",filename,str(e)
-            return e
-        return (filename[:-5])
+def analysis(filename,myWorker):
+    try:
+        hSum=ROOT.TH1F("hEdep","E_{dep}",3000,0,300)
+        hSum.GetXaxis().SetTitle("E_{dep} [MeV]")
+        hSum.GetYaxis().SetTitle("rel. freq.")
+        outfile=ROOT.TFile(filename[:-5]+"-histos.root","RECREATE")
+        dir=outfile.mkdir(filename[:-5])
+        dir.cd()
+        infile=ROOT.TFile(filename,"READ")
+        data=infile.Get("sim")
+        dir.cd()
+        for event in data:
+            sum=0
+            for hit in event.Detector:
+                sum+=hit.edep
+            if(sum>5.):
+                hSum.Fill(sum)
+        hSum.Write()
+        outfile.Write()
+        outfile.Close()
+    except Exception as e:
+        print "Problem in file:",filename,str(e)
+        return e
+    return (filename[:-5])
     
 if __name__=="__main__":
     ROOT.gSystem.Load("libAnalysis")
     myAnalysis=exampleAnalysis()
     myAnalysis.Init()
-    todo=myAnalysis(function=exampleAnalysis.analysis)
+    todo=myAnalysis(function=analysis)
     while len(todo):
         todo=myAnalysis(function=analysis)
-    infile=ROOT.TFile(self.outfile)
-    materials=["G4_GRAPHITE","G4_Al","G4_Mg"]
+    infile=ROOT.TFile(myAnalysis.args.output,"UPDATE")
+    dir=infile.mkdir("plots")
+    materials=["G4_GRAPHITE","G4_Al","G4_Mg","G4_AIR"]
     energies=[100,135,150,200,235,250,270]
     lengths=[5]
     iCol=0
     for material in materials:
+        t=[]
+        ed=[]
+        w=[]
         for e in energies:
-            x=[]
-            y=[]
-            dy=[]
-            w=[]
-            dw=[]
-            for d in lengths:
-                makePlots(infile, material, e, d)
-                
+            ie,ide,iw=fitDir(infile, material+"-"+str(e)+"-5")
+            if ie!=None:
+                t.append(e)
+                ed.append(ie)
+                w.append(iw)
+        dir.cd()
+        makePlots(material+"5-",t,ed,w)       
+    t=[]
+    ed=[]
+    w=[]
     for e in energies:
-        makePlots(infile,"G4_GRAPHITE",e,10)       
+        ie,ide,iw=fitDir(infile, "G4_GRAPHITE-"+str(e)+"-10")
+        if ie!=None:
+            t.append(e)
+            ed.append(ie)
+            w.append(iw)
+    dir.cd()
+    makePlots("G4_GRAPHITE-10-",t,ed,w)
--- scripts/TestBeam2016BEnergyCalibration/TestBeam2016BEnergyCalibration.sh
+++ scripts/TestBeam2016BEnergyCalibration/TestBeam2016BEnergyCalibration.sh
@@ -7,10 +7,11 @@ if [ $# -eq 1 ]
 fi
 DIR=../$NAME-$(date "+%Y-%m-%d")
 mkdir -p $DIR
-nice -n 15 ./polarimeterstudies -g testbeam2016b -m scripts/$NAME/$NAME.mac -n $N 1>out.log 2>err.log
+nice -n 15 ./polarimeterstudies -g testbeam2016b -m scripts/$NAME/init.mac -n $N 1>out.log 2>err.log
 mv *.root $DIR/
 cp scripts/AnalysisBase.py $DIR/.
 cp libAnalysis* $DIR/.
 cp scripts/$NAME/$NAME.py $DIR/.
 cd $DIR
 ./$NAME.py -o $NAME.root -n $N 
+mail -s "$NAME finished at $(date)" maanen@physik.rwth-aachen.de </dev/null
