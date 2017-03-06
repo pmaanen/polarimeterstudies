30c2ee45431a2a3ba196e813ad096dbaf66f60b0
pmaanen <paul.maanen@rwth-aachen.de>
2017-03-06 08:50:51 +0100

    Replaced global variables with calls to config manager - part 2
    Did replacement in files missing from last commit

--- src/JediRun.cc
+++ src/JediRun.cc
@@ -8,7 +8,7 @@
 #include <JediRun.hh>
 #include "Analysis.hh"
 #include <G4Threading.hh>
-#include <JediCommon.hh>
+#include <JediConfigurationManager.hh>
 #include <memory>
 
 JediRun::JediRun():G4Run() {}
@@ -21,12 +21,12 @@ void JediRun::Merge(const G4Run* aRun) {
 	auto an=Analysis::Instance();
 	auto simEvents=an->getSimEvents();
 	auto genEvents=an->getGenEvents();
-	if(gVerbose>2)
+	if(JediConfigurationManager::Instance()->GetVerbose()>2)
 		G4cout<<"JediRun::Merge: Thread "<<G4Threading::G4GetThreadId()<<" has "<<simEvents->size()<<" sim events.";
 	for(auto iEvent : *simEvents){
 		fSimEvents.push_back(iEvent);
 	}
-	if(gVerbose>2)
+	if(JediConfigurationManager::Instance()->GetVerbose()>2)
 			G4cout<<"JediRun::Merge: Thread "<<G4Threading::G4GetThreadId()<<" has "<<genEvents->size()<<" gen events.";
 	for(auto iEvent : *genEvents){
 		fGenEvents.push_back(iEvent);
